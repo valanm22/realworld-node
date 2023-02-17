f5e33a3b67e51e1a0146bc2f3e4117d7e3cfd38a
Willian Pinheiro <willpinhaaa@gmail.com>
2023-02-17 18:35:11 -0300

    Fix GitHub actions .env access (#22)

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,13 +1,13 @@
-import { str, host, port } from "envalid";
+import { str, host, port, testOnly } from "envalid";
 
 export const specs = {
     NODE_ENV: str({ choices: ["development", "production", "test"] }),
 
     PG_HOSTNAME: host({ devDefault: "localhost" }),
     PG_PORT: port({ default: 5432 }),
-    PG_USERNAME: str(),
-    PG_PASSWORD: str(),
-    PG_NAME: str(),
+    PG_USERNAME: str({ devDefault: testOnly("not required") }),
+    PG_PASSWORD: str({ devDefault: testOnly("not required") }),
+    PG_NAME: str({ devDefault: testOnly("not required") }),
 
     SERVER_PORT: port({ devDefault: 3000 }),
 };
