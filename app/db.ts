ad7cdbbde379a0edab8d60da500ca29f8e0a7569
Willian Pinheiro <willpinhaaa@gmail.com>
2023-02-17 14:36:59 -0300

    Add environment variables (#18)

--- app/db.ts
+++ app/db.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 const db = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "willpinha",
-    password: "12066464lL",
-    database: "test",
+    host: process.env.DB_HOSTNAME,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [ __dirname + "/models/*.{js,ts}"],
     synchronize: true
 });
--- app/index.ts
+++ app/index.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import app from "./app";
 import db from "./db";
 
@@ -5,10 +9,13 @@ db.initialize()
     .then(() => {
         console.log("Database initialized");
     })
+    .then(() => {
+        const serverPort = process.env.SERVER_PORT || 8080;
+
+        app.listen(serverPort, () => {
+            console.log("Server is running on port", serverPort);
+        });
+    })
     .catch((error) => {
         console.log("Database initialization error:", error);
     });
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
